a24286a5c0f6a230e4098111e5f69e85163b823a
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2022-03-14 19:51:16 +0800

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,3 @@
+document.writeln("<center>");
+document.writeln("<a href=\"https://meiliwuya.github.io/rxwg.html\" target=\"_blank\"><img class=\"ue-image\" src=\"https://ae01.alicdn.com/kf/H0a12e3c838ea45cb976c21cdf4758e33h.png\" width=\"136\" height=\"40\"/></a>");
+document.writeln("</center>");
