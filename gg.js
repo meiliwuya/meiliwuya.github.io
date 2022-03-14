029988c60076bdb5a8a4187429e4bd294bdb1c92
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2022-03-15 00:06:59 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,3 +1,3 @@
 document.writeln("<center>");
-document.writeln("<a href=\"http://www.meiliwuya.com/?cate=1\" target=\"_blank\"><img class=\"ue-image\" src=\"https://ae01.alicdn.com/kf/H571b1bca992e4704bb1ffefda1b88ab3G.gif\" width=\"400\" height=\"180\"/></a>");
+document.writeln("<a href=\"http://www.meiliwuya.com/?cate=1\" target=\"_blank\"><img class=\"ue-image\" src=\"https://ae01.alicdn.com/kf/H924667bb6973466b86dc6b8fa23c019c2.gif\" width=\"300\" height=\"120\"/></a>");
 document.writeln("</center>");
