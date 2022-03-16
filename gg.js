6b3f44c572b6544da9f89d802bb6003dd2f01adb
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2022-03-17 02:52:03 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,3 +1,6 @@
+document.writeln("<p>");
+document.writeln("    &nbsp; &nbsp; &nbsp;<span style=\"font-size: 10px; text-decoration: underline;\">&nbsp; &nbsp; &nbsp;<a href=\"http://www.meiliwuya.com/?id=210\" target=\"_blank\" style=\"font-size: 10px; text-decoration: underline;\">网站使用指南</a></span><br/> &nbsp; &nbsp; &nbsp;<span style=\"font-size: 14px; text-decoration: underline;\"><a href=\"http://www.meiliwuya.com/?cate=1\" target=\"_blank\" style=\"font-size: 14px; text-decoration: underline;\">更多精彩影视</a></span>&nbsp; &nbsp; &nbsp;<a href=\"http://meiliwuya.com/av2/tt.html\" target=\"_blank\" style=\"font-size: 14px; text-decoration: underline;\"><span style=\"font-size: 14px;\">sexy音乐欣赏</span></a>");
+document.writeln("</p>");
 document.writeln("<center>");
-document.writeln("<a href=\"http://www.meiliwuya.com/?cate=1\" target=\"_blank\"><img class=\"ue-image\" src=\"http://meiliwuya.com/gg/001.gif\" width=\"250\" height=\"80\"/></a>");
+document.writeln("    <a href=\"http://www.meiliwuya.com/?cate=1\" target=\"_blank\"><img class=\"ue-image\" src=\"http://meiliwuya.com/gg/001.gif\" width=\"250\" height=\"80\"/></a>");
 document.writeln("</center>");
