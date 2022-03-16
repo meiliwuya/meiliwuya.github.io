a5af5719733247aafce2f7b37f8ef88be26b5b44
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2022-03-17 00:19:48 +0800

    Create gg2.js

---
+++ gg2.js
@@ -0,0 +1,4 @@
+document.writeln("    <img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"40\" height=\"20\">");
+document.writeln("		 <font face=\"雅黑\" color=\"#DDDDDD\" size=\"2\"><img src=\"https://ae01.alicdn.com/kf/H71611b91b37d42e9b785feead839d32e8.jpg\" width=\"12\" height=\"12\"/> 分享给好友</font>");
+document.writeln("	<br>");
+document.writeln("	<img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"60\" height=\"90\"><img src=\"https://ae01.alicdn.com/kf/H9b6fee6b229f4ea9961e12d48b24c1a23.png\" width=\"90\" height=\"80\">");
