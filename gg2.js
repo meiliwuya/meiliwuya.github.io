d2476dfbadaac7e3d549fb81a7bb6c3b736b3da8
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2022-03-24 02:17:00 +0800

    Update gg2.js

--- gg2.js
+++ gg2.js
@@ -1,8 +1,9 @@
-document.writeln("	<hr>");
-document.writeln("    <img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"40\" height=\"20\">");
-document.writeln("		 <font face=\"雅黑\" color=\"#999999\" size=\"2\"><img src=\"https://ae01.alicdn.com/kf/H71611b91b37d42e9b785feead839d32e8.jpg\" width=\"12\" height=\"12\"/> 分享码-微信扫一扫-听音乐看大片</font>");
-document.writeln("	<br>");
-document.writeln("	<img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"60\" height=\"75\"><img src=\"https://ae01.alicdn.com/kf/H9b6fee6b229f4ea9961e12d48b24c1a23.png\" width=\"70\" height=\"70\">");
-document.writeln("	<center>");
-document.writeln("	<img src=\"https://ae01.alicdn.com/kf/H5054f1548dd94d769e1623c8e9a99298B.gif\" width=\"200\" height=\"65\">");
-document.writeln("	</center>");
+	<hr>
+    <img src="https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png" width="40" height="20">
+		 <font face="雅黑" color="#999999" size="1"><img src="https://ae01.alicdn.com/kf/H71611b91b37d42e9b785feead839d32e8.jpg" width="12" height="12"/> 分享给好友-微信扫一扫-听音乐看大片</font>
+	<br>
+	<img src="https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png" width="60" height="78"><img src="https://ae01.alicdn.com/kf/H9b6fee6b229f4ea9961e12d48b24c1a23.png" width="70" height="70">
+	<center>
+	<img src="https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png" width="0" height="40">
+	<img src="https://ae01.alicdn.com/kf/H04c1fd6397444221be009ab54450d81an.png" width="150" height="30">
+	</center>
