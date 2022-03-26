a481d9a335237401f569b913a5df85d439c5d967
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2022-03-26 15:42:47 +0800

    Update gg2.js

--- gg2.js
+++ gg2.js
@@ -1,6 +1,6 @@
 document.writeln("	<hr>");
 document.writeln("    <img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"40\" height=\"20\">");
-document.writeln("		 <font face=\"雅黑\" color=\"#999999\" size=\"1\"><img src=\"https://ae01.alicdn.com/kf/H71611b91b37d42e9b785feead839d32e8.jpg\" width=\"12\" height=\"12\"/> 分享给好友-微信扫一扫-听音乐看大片</font>");
+document.writeln("		 <font face=\"雅黑\" color=\"#999999\" size=\"1\"><img src=\"https://ae01.alicdn.com/kf/H71611b91b37d42e9b785feead839d32e8.jpg\" width=\"12\" height=\"12\"/> 分享给好友-微信扫一扫</font>");
 document.writeln("	<br>");
 document.writeln("	<img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"60\" height=\"78\"><img src=\"https://ae01.alicdn.com/kf/H9b6fee6b229f4ea9961e12d48b24c1a23.png\" width=\"70\" height=\"70\">");
 document.writeln("	<center>");
