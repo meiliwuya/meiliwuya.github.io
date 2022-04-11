314291ba16f0f7fc69e053a28a07d681bd677bd7
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2022-04-11 22:04:16 +0800

    Update gg2.js

--- gg2.js
+++ gg2.js
@@ -5,5 +5,5 @@ document.writeln("	<br>");
 document.writeln("	<img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"60\" height=\"78\"><img src=\"https://ae01.alicdn.com/kf/H9b6fee6b229f4ea9961e12d48b24c1a23.png\" width=\"70\" height=\"70\">");
 document.writeln("	<center>");
 document.writeln("	<img src=\"https://s3.bmp.ovh/imgs/2021/09/b4dcb6343d07344e.png\" width=\"0\" height=\"40\">");
-document.writeln("	<a href=\"http://meiliwuya.com/\" target=\"parent\" ><img src=\"https://ae01.alicdn.com/kf/H04c1fd6397444221be009ab54450d81an.png\" width=\"150\" height=\"30\"></a>");
+document.writeln("	<a href=\"http://42.192.251.136:81/\" target=\"parent\" ><img src=\"https://ae01.alicdn.com/kf/H04c1fd6397444221be009ab54450d81an.png\" width=\"150\" height=\"30\"></a>");
 document.writeln("	</center>");
